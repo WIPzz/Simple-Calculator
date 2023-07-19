9ed3f93f8ef5406f6b2b24ccf79d05b0b446cda2
WIPzz <139990034+WIPzz@users.noreply.github.com>
2023-07-19 16:41:25 +0200

    Add files via upload

---
+++ Test123/Test123/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test123
+{
+    public partial class Form1 : Form
+    {
+        double num1 = 0;
+        double num2 = 0;
+        double resultNum = 0;
+        string oper = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Number Button
+        private void numBttn(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+                textBox1.Clear();
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        // Operator Button
+        private void operatorBttn(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            num1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Text = textBox1.Text + button.Text;
+            oper = textBox1.Text;
+            textBox3.Text = oper;
+            textBox1.Clear();
+        }
+
+        // Equal Button
+        private void resultBttn(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            num2 = Convert.ToDouble(textBox1.Text);
+            textBox2.Clear();
+            if (oper == "+")
+                resultNum = num1 + num2;
+            else if (oper == "-")
+                resultNum = num1 - num2;
+            else if (oper == "*")
+                resultNum = num1 * num2;
+            else resultNum = num1 / num2;
+            textBox2.Text = resultNum.ToString();
+
+        }
+
+        // Clear Button
+        private void clearBttn(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            num1 = 0;
+            num2 = 0;
+            resultNum = 0;
+            oper = "";
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
